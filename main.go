cd72509abba37e0124d4d935b46834c391b72270
zare926 <ilove.boc926@gmail.com>
2022-01-05 09:06:40 +0900

    logsetting

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"todo_app/config"
-)
-
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+
+	// log.Println("test")
 }
